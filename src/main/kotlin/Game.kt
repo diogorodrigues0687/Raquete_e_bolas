f340690685aa2a577bb67817e63a2b5c1074739d
diogohsr06 <diogohsr2006@gmail.com>
2025-11-25 09:44:13 +0000

    SUIIII

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -6,7 +6,7 @@ data class Game(val area: Area, val balls: List<Ball>, val racket: Racket)
 /**Função que desenha todos os objetos do jogo **/
 fun drawGame(canvas: Canvas, game: Game) {
     canvas.erase()
-    canvas.drawImage("WindowsXP.jpg",0,0,width,height) //Background personalizado, comentar esta linha se necessário
+    //canvas.drawImage("WindowsXP.jpg",0,0,width,height) //Background personalizado, comentar esta linha se necessário
     for (b in game.balls)
         drawBall(canvas, b)
     drawRacket(canvas, game.racket)
